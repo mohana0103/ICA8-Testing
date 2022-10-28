7a1c0ff360a9afbe2901b98623183d435d5d3a92
mohana0103 <mkaruma1@asu.edu>
2022-10-27 18:59:58 -0700

    Pushing skeleton Files

---
+++ src/main/java/org/example/urinals.java
@@ -0,0 +1,8 @@
+package org.example;
+//Author Mohana Deepthi Karumanchi
+
+public class urinals {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
